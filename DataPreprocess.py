b4125ab945f023b891b7d2be447da8548c7ef0d9
Pratik Panchal <pratik.sp.1112@gmail.com>
2020-09-30 17:57:11 +0530

    data Resize part added

---
+++ DataPreprocess.py
@@ -0,0 +1,32 @@
+import os
+import json
+from tqdm import tqdm
+from PIL import Image
+
+with open("config.json", "r") as f:
+    dataset = json.load(f)['DatasetDetails']
+
+def resizeImagesTo128x128():
+    picsDirectory = os.path.join(dataset['DatasetRootPath'], dataset['DatasetImages']).replace("/", "\\")
+    picsProcessedDirectory = os.path.join(dataset['DatasetRootPath'], dataset['DatasetProcessedImages']).replace("/", "\\")
+    
+    print(picsProcessedDirectory)
+
+    if not os.path.exists(picsProcessedDirectory):
+        os.makedirs(picsProcessedDirectory)
+
+        originalWidth = 178
+        originalHeight = 218
+        difference = (originalHeight - originalWidth) // 2
+
+        newWidth = 128
+        newHeight = 128
+
+        cropRect = (0, difference, originalWidth, originalHeight - difference)
+
+        for eachImage in tqdm(os.listdir(picsDirectory)):
+            image = Image.open(os.path.join(picsDirectory, eachImage)).crop(cropRect)
+            image.thumbnail((newWidth, newHeight), Image.ANTIALIAS)
+            image.save(os.path.join(picsProcessedDirectory, eachImage))
+
+resizeImagesTo128x128()
