e6f9c2234b55e92b5e38c41f0402159be62d27c8
psp2108 <43181107+psp2108@users.noreply.github.com>
2020-11-15 14:39:57 +0530

    Pratik (#9)
    * Generator model
    
    * Discriminator
    
    * gan model code written
    
    * error solved
    
    * Folder restructuring
    
    * git ignore
    
    * gan update
    
    * Gan model running
    
    * Training code working fine
    
    * code to load model
    
    * restructure
    
    * Some parameters added
    
    * update
    
    * Creating, Training, and Loading a GAN model
    
    * Removed extra imports
    
    * code optimization
    
    * Removed kachra from gitignore
    
    * git ignore
    
    * -1 to 0
    
    * -1 to 0
    
    * variable names changed
    
    Co-authored-by: Prem Bhajaj <prembhajaj@gmail.com>

--- src/DataPreprocess.py
+++ src/DataPreprocess.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 with open("config.json", "r") as f:
     jsonFile = json.load(f)
-    CSVDetails = jsonFile['CSVDetails']
+    csvDetails = jsonFile['CSVDetails']
     dataset = jsonFile['ImageDetails']
     deleteRecords = jsonFile['DeleteRecords']
     
@@ -38,26 +38,41 @@ def resizeImagesTo128x128():
         print("Output folder '"+ picsProcessedDirectory +"' aready exists. Images are either already resized or delete the folder.")
     print("-" * 100)
 
-# 2. Combine Excels to single CSV
+# 2. Normalize values
+def normalizeAttributesFile():
+    print("Normalizing Data ...")
+    csvRootPath = csvDetails['CSVRootPath']
+    toNormalize = csvDetails['NormalizeData']['AttributesFile']
+    df = pd.read_csv(os.path.join(csvRootPath, toNormalize['From']).replace("/", "\\"))
+
+    rawData = df.values
+    rawData[:,1:] = (rawData[:,1:]+1)/2
+    df = pd.DataFrame(rawData, columns=df.columns)
+   
+    df.to_csv(os.path.join(csvRootPath, toNormalize['To']).replace("/", "\\"), index = False)
+
+    print(toNormalize['From'], "normalized")
+    print("-" * 100)
+
+# 3. Combine Excels to single CSV
 def combineExcels(): 
     print("Combining CSV files ...")
-    CSVRootPath = CSVDetails['CSVRootPath']
-    CSVList = CSVDetails['CSVList']
-    combinedCSV = CSVDetails['CombinedCSV']
-
-    if len(CSVList) == 0:
+    csvRootPath = csvDetails['CSVRootPath']
+    csvList = csvDetails['CSVListToCombine']
+    combinedCSV = csvDetails['CombinedCSV']
+    if len(csvList) == 0:
         print("No files selected to combine")
         return
 
-    df = pd.read_csv(os.path.join(CSVRootPath, CSVList[0]).replace("/", "\\"))
-    print("Reading file '"+ CSVList[0] +"'")
+    df = pd.read_csv(os.path.join(csvRootPath, csvList[0]).replace("/", "\\"))
+    print("Reading file '"+ csvList[0] +"'")
 
-    for i in range(1,len(CSVList)):
-        print("Reading file '"+ CSVList[i] +"'")
-        CSVFile = pd.read_csv(os.path.join(CSVRootPath, CSVList[i]).replace("/", "\\"))
-        df = df.merge(CSVFile, how = 'outer', on = 'image_id')
+    for i in range(1,len(csvList)):
+        print("Reading file '"+ csvList[i] +"'")
+        csvFile = pd.read_csv(os.path.join(csvRootPath, csvList[i]).replace("/", "\\"))
+        df = df.merge(csvFile, how = 'outer', on = 'image_id')
 
-    df.to_csv(os.path.join(CSVRootPath, combinedCSV).replace("/", "\\"), index = False)
+    df.to_csv(os.path.join(csvRootPath, combinedCSV).replace("/", "\\"), index = False)
 
     print("Files combined")
     print("-" * 100)
@@ -65,11 +80,11 @@ def combineExcels():
 # 3. Remove Improper Records
 def deleteImproperRecords():
     print("Deleting Improper Records ...")
-    CSVRootPath = CSVDetails['CSVRootPath']
-    combinedCSV = CSVDetails['CombinedCSV']
+    csvRootPath = csvDetails['CSVRootPath']
+    combinedCSV = csvDetails['CombinedCSV']
     deleteRecordList = deleteRecords['RecordList']
 
-    df = pd.read_csv(os.path.join(CSVRootPath, combinedCSV).replace("/", "\\"))    
+    df = pd.read_csv(os.path.join(csvRootPath, combinedCSV).replace("/", "\\"))    
     columnList = deleteRecordList.keys()
 
     for i in columnList:
@@ -78,16 +93,17 @@ def deleteImproperRecords():
 
     print("Dropping columns {}".format(columnList))
     df.drop(columnList, axis = 1, inplace = True) 
-    df.to_csv(os.path.join(CSVRootPath, combinedCSV).replace("/", "\\"), index = False)
+    df.to_csv(os.path.join(csvRootPath, combinedCSV).replace("/", "\\"), index = False)
     
     print("Records Deleted")
     print("-" * 100)
 
-# 4. Manual Filtering 
+# 5. Manual Filtering 
 def selectiveDelete():
     pass
 
 resizeImagesTo128x128()
+normalizeAttributesFile()
 combineExcels()
 deleteImproperRecords()
 selectiveDelete()
