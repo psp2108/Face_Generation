937037436621d268fd0a220b52be26752af21766
psp2108 <43181107+psp2108@users.noreply.github.com>
2021-03-03 10:24:45 -0800

    Pratik (#12)
    * Generator model
    
    * Discriminator
    
    * gan model code written
    
    * error solved
    
    * Folder restructuring
    
    * git ignore
    
    * gan update
    
    * Gan model running
    
    * Training code working fine
    
    * code to load model
    
    * restructure
    
    * Some parameters added
    
    * update
    
    * Creating, Training, and Loading a GAN model
    
    * Removed extra imports
    
    * code optimization
    
    * Removed kachra from gitignore
    
    * git ignore
    
    * -1 to 0
    
    * -1 to 0
    
    * variable names changed
    
    * merging config sample
    
    * Merge Attributes, Manual Deletion and Drop columns
    
    * code cleaning and removing the pandas warning
    
    * variable name change
    
    * config
    
    * load generator
    
    * current stable model
    
    * test
    
    * train model
    
    * 7
    
    * extra logs removed
    
    * model diagrams
    
    * turned off axis lines while displaying image in loadmodel
    
    * folder generation
    
    * rmsprop
    
    * logging in gan.py
    
    * clipvalue=1.0, decay=1e-8
    
    * comments
    
    * typo
    
    Co-authored-by: Prem Bhajaj <prembhajaj@gmail.com>

--- src/Generator.py
+++ src/Generator.py
@@ -9,30 +9,35 @@ with open("config.json", "r") as f:
 def getGeneratorModel():
     numberOfFeatures = modelDetails['TotalAttributes']
     attributesInput = layers.Input(shape=(numberOfFeatures,))
-    attributes = layers.Dense(4 * 4 * numberOfFeatures)(attributesInput)
-    attributes = layers.Reshape([4, 4, numberOfFeatures])(attributes)
+    attributes = layers.Dense(16 * 16 * 116)(attributesInput)
+    attributes = layers.LeakyReLU()(attributes)
+    attributes = layers.Reshape([16, 16, 116])(attributes)
 
     randomVectorSize = modelDetails['RandomVectorSize']
     randomNoiseInput = layers.Input(shape=(randomVectorSize,))
-    randomNoise = layers.Dense(4 * 4 * 256)(randomNoiseInput)
-    randomNoise = layers.Reshape([4, 4, 256])(randomNoise)
+    randomNoise = layers.Dense(16 * 16 * 12)(randomNoiseInput)
+    randomNoise = layers.LeakyReLU()(randomNoise)
+    randomNoise = layers.Reshape([16, 16, 12])(randomNoise)
 
     merged = layers.Concatenate()([randomNoise, attributes])
-    generator = layers.BatchNormalization()(merged)
-    # 4x4 => 8x8
-    generator = layers.Conv2DTranspose(128, (5,5), (2,2), padding="same")(generator)
-    generator = layers.LeakyReLU(alpha=0.2)(generator)
-    # 8x8 => 16x16
-    generator = layers.Conv2DTranspose(128, (5,5), (2,2), padding="same")(generator)
-    generator = layers.LeakyReLU(alpha=0.2)(generator)
+    generator = layers.Conv2D(256, (5,5), padding="same")(merged)
+    generator = layers.LeakyReLU()(generator)
+    
     # 16x16 => 32x32
-    generator = layers.Conv2DTranspose(128, (5,5), (2,2), padding="same")(generator)
-    generator = layers.LeakyReLU(alpha=0.2)(generator)
+    generator = layers.Conv2DTranspose(256, (4,4), (2,2), padding="same")(generator)
+    generator = layers.LeakyReLU()(generator)
     # 32x32 => 64x64
-    generator = layers.Conv2DTranspose(128, (5,5), (2,2), padding="same")(generator)
-    generator = layers.LeakyReLU(alpha=0.2)(generator)
+    generator = layers.Conv2DTranspose(256, (4,4), (2,2), padding="same")(generator)
+    generator = layers.LeakyReLU()(generator)
     # 64x64 => 128x128
-    generator = layers.Conv2DTranspose(3, (5,5), (2,2), padding="same", activation="selu")(generator)
+    generator = layers.Conv2DTranspose(256, (4,4), (2,2), padding="same")(generator)
+    generator = layers.LeakyReLU()(generator)
+
+    generator = layers.Conv2D(512, (5,5), padding="same")(generator)
+    generator = layers.LeakyReLU()(generator)
+    generator = layers.Conv2D(512, (5,5), padding="same")(generator)
+    generator = layers.LeakyReLU()(generator)
+    generator = layers.Conv2D(3, (7,7), padding="same", activation='tanh')(generator)
 
     generatorModel = keras.models.Model([attributesInput, randomNoiseInput], generator)
 
