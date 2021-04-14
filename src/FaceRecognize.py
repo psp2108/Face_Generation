d7564319fb75d2fe76638de372b994f0f19cf0bb
Pratik Panchal <pratik.sp.1112@gmail.com>
2021-04-14 22:29:42 +0530

    change in code and used hog model to which enable CPU utilization

--- src/FaceRecognize.py
+++ src/FaceRecognize.py
@@ -1,4 +1,3 @@
-import face_recognition as fr
 import os
 import cv2
 import face_recognition
@@ -12,42 +11,44 @@ class FaceIdentifier():
         self.imagesPath = os.path.join(self.basePath, "imgs")
         self.detailsPath = os.path.join(self.basePath, "details")
         self.testPath = os.path.join(self.basePath, "to test")
+        self.tolerance = 0.6
+        self.model = "hog" # or "cnn"
 
-        self.encoded = {}
+        self.known_faces = []
+        self.known_names = []
 
-        for dirpath, dnames, fnames in os.walk(self.imagesPath):
-            for f in fnames:
-                if f.endswith(".jpg") or f.endswith(".png"):
-                    face = fr.load_image_file(os.path.join(self.imagesPath, f))
-                    encoding = fr.face_encodings(face)[0]
-                    self.encoded[f.split(".")[0]] = encoding
-        
-        self.faces_encoded = list(self.encoded.values())
-        self.known_face_names = list(self.encoded.keys())
+        for name in os.listdir(self.imagesPath):
+            for fileName in os.listdir(os.path.join(self.imagesPath, name)):
+                image = face_recognition.load_image_file(os.path.join(self.imagesPath, name, fileName))
+                encoding = face_recognition.face_encodings(image)[0]
+
+                self.known_faces.append(encoding)
+                self.known_names.append(name)
 
     def getFaceID(self, imagePath = None, image = None):
         if imagePath:
             image = cv2.imread(imagePath, 1)
 
-        face_locations = face_recognition.face_locations(image)
-        unknown_face_encodings = face_recognition.face_encodings(image, face_locations)
+        location = face_recognition.face_locations(image, model=self.model)
+        if len(location) == 0:
+            return "none"
 
-        face_names = []
-        for face_encoding in unknown_face_encodings:
-            # See if the face is a match for the known face(s)
-            matches = face_recognition.compare_faces(self.faces_encoded, face_encoding)
-            name = "none"
+        encoding = face_recognition.face_encodings(image, location)[0]
+        location = location[0]
 
-            # use the known face with the smallest distance to the new face
-            face_distances = face_recognition.face_distance(self.faces_encoded, face_encoding)
-            best_match_index = np.argmin(face_distances)
-            if matches[best_match_index]:
-                name = self.known_face_names[best_match_index]
+        IDs = []
 
-            face_names.append(name)
-        
-        return face_names
+        results = face_recognition.compare_faces(self.known_faces, encoding, self.tolerance)
 
+        if True in results:
+            for index in range(len(results)):
+                if results[index]:
+                    IDs.append(self.known_names[index])
+
+            return IDs
+        else:
+            return ["none"]
+                
     def getDetails(self, _id):      
         jsonFileName = "{}.json"
         jsonFilePath = os.path.join(self.detailsPath, jsonFileName).format(_id)
